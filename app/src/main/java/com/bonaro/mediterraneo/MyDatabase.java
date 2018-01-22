ae08c68d6cec6a656951b48aeb615953d3aa0805
José Lozano Hernández <jlozanoher@gmail.com>
2018-01-22 16:40:49 -0100

    bd change on no gluten dish

--- app/src/main/java/com/bonaro/mediterraneo/MyDatabase.java
+++ app/src/main/java/com/bonaro/mediterraneo/MyDatabase.java
@@ -10,7 +10,7 @@ import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 public class MyDatabase extends SQLiteAssetHelper {
 
     private static final String DATABASE_NAME = "db_mediterraneo.sqlite";
-    private static final int DATABASE_VERSION = 3;
+    private static final int DATABASE_VERSION = 4;
 
     public MyDatabase(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
